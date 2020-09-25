33d87774fde19792e649244e92e8e5781c00a981
上帝派到人间脑操作系统管理员 <1872220587@qq.com>
2020-09-25 14:52:10 +0800

    先确定方向在努力

--- study.economics.sh
+++ study.economics.sh
@@ -1,20 +1,17 @@
-一、修改IP地址
+#修改网卡
 vi /etc/sysconfig/network-scripts/ifcfg-ens33
-
-二、修改网关
-vi /etc/sysconfig/network
-
-DNS：
-vi /etc/resolv.conf
-echo "nameserver 114.114.114.114" >>/etc/resolv.con
-
-
-修改网卡
-vi /etc/sysconfig/network-scripts/ifcfg-ens33
-
 systemctl restart network    #重启网卡
-
 ping www.baidu.com
+
+
+#VMware设置centos7共享文件夹
+输入命令vmware-hgfsclient查看共享目录是否已经设置成功
+vmware-hgfsclient
+mkdir -p /root/os
+echo ".host:/os /root/os                        fuse.vmhgfs-fuse allow_other,defaults 0 0" >>/etc/fstab
+mount -a
+#卸载共享目录cd  /mnt/project
+umount /root/os
 
 
 #####################使用—>制作镜像->停止容器—>删除容器->上传镜像->运行镜像容器->  每天进步一点点
@@ -30,7 +27,8 @@ docker run \
 -p 888:888 \
 -p 3306:3306 \
 -p 8888:8888 \
--v /root/www:/www \
+-p 6379:6379 \
+-v /root/os/www:/www \
 centos:7.8.2003
 docker exec -it bt /bin/bash   #使用
 yum install -y wget && wget -O install.sh http://download.bt.cn/install/install_6.0.sh && sh install.sh
@@ -49,17 +47,22 @@ bt 1
 /etc/init.d/redis restart
 
 #日常维护
-docker commit  -a "1872220587" -m "针对性学习" bt 1872220587/study:mysql  #提交
+docker commit  -a "1872220587" -m "针对性学习" bt 1872220587/study:economics.v1  #提交
 docker restart bt #重启
 docker stop bt  #停止
 docker rm bt    #删除
-docker rm -f 3013eb725f5b  #强制删除
-rm -rf /var/lib/docker #删除镜像/容器等
-chmod 777 -R /var/lib/docker/overlay2/85546a1efe483b276a4bfa7e9c573550567b55d8b9071ea9961d4093904603a8/ #给一个目录及其子目录所有文件添加权限
-docker push 1872220587/study:mysql   #上传
-docker pull 1872220587/study:mysql   #下载
+docker rm -f  bt #强制删除
+#删除锁定文件
+chattr -i .user.ini        #解除锁定
+chmod -R 777 .user.ini #赋777权限
+rm -rf .user.ini          #删除文件
 
+chattr -i btwaf.conf       #解除锁定
+chmod -R 777 btwaf.conf #赋777权限
+rm -rf btwaf.conf    #删除文件
 
+docker push  1872220587/study:economics.v1   #上传
+docker pull  1872220587/study:economics.v1  #下载
 
 
 #删除所有未被容器使用的镜像:
@@ -70,3 +73,5 @@ docker container prune
 
 #删除所有未被挂载的卷:
 docker volume prune
+
+
