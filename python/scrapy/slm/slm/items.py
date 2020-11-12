20bfd338a673a495233fe7f4b002df1dc8c270b5
上帝派到人间脑操作系统管理员 <1872220587@qq.com>
2020-11-12 16:13:17 +0800

    scrapy

--- python/scrapy/slm/slm/items.py
+++ python/scrapy/slm/slm/items.py
@@ -6,7 +6,12 @@
 import scrapy
 
 
-class SlmItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class DoubanMovieItem(scrapy.Item):
+    # 排名
+    ranking = scrapy.Field()
+    # 电影名称
+    movie_name = scrapy.Field()
+    # 评分
+    score = scrapy.Field()
+    # 评论人数
+    score_num = scrapy.Field()
